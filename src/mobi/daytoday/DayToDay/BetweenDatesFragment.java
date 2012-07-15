d798fa6a0f49af9cd0ba8ab87f47702f1987ac52
Doyle Young <dyoung@gmail.com>
2012-07-14 21:29:37 -0400

    Added months and natural language option on between dates tab, switched "Answer in" to a spinner.  Began implementation of a human like age (all commented out)

--- src/mobi/daytoday/DayToDay/BetweenDatesFragment.java
+++ src/mobi/daytoday/DayToDay/BetweenDatesFragment.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2012 Doyle Young
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2012 Doyle Young
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package mobi.daytoday.DayToDay;
 
 import java.text.ParseException;
@@ -27,28 +27,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Switch;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
 /**
- * Gather two dates and the form of the response (days or weeks) and report the 
- * number of days or weeks between the dates
+ * Gather two dates and the form of the response and report the time between the
+ * dates
+ * 
  * @author Doyle Young
- *
+ * 
  */
-public class BetweenDatesFragment extends SherlockFragment implements OnDateSetListener {
+public class BetweenDatesFragment extends SherlockFragment implements
+    OnDateSetListener {
   private static final String TAG = "BetweenDatesFragment";
-  
+
+  private enum AnswerInType {
+    DAYS, WEEKS, MONTHS, NATURAL, AGE
+  }
+
   private EditText firstDateInput;
   private EditText secondDateInput;
-  private Object daysWeeksSwitch;
+  private Spinner answerInSpinner;
+  private AnswerInType answerType;
   private TextView answer;
   private ImageButton firstDateSelect;
   private ImageButton secondDateSelect;
@@ -58,109 +67,127 @@ public class BetweenDatesFragment extends SherlockFragment implements OnDateSetL
   private String secondDate;
   private boolean firstActive;
   private boolean secondActive;
-  
+
   /*
    * Handles click on the first date select button
    */
   private OnClickListener firstDateListener = new OnClickListener() {
     public void onClick(View v) {
       FragmentTransaction ft = getFragmentManager().beginTransaction();
-      
-      Fragment prev = getFragmentManager().findFragmentByTag(DatePickerDialogFragment.DATE_PICKER_ID);
+
+      Fragment prev = getFragmentManager().findFragmentByTag(
+          DatePickerDialogFragment.DATE_PICKER_ID);
       if (prev != null) {
         ft.remove(prev);
         firstActive = secondActive = false;
       }
       ft.addToBackStack(null);
-      
-      DialogFragment frag = new DatePickerDialogFragment(BetweenDatesFragment.this);
+
+      DialogFragment frag = new DatePickerDialogFragment(
+          BetweenDatesFragment.this);
       firstActive = true;
       frag.show(ft, DatePickerDialogFragment.DATE_PICKER_ID);
     }
   };
- 
+
   /*
    * Handles click on the second date select button
    */
   private OnClickListener secondDateListener = new OnClickListener() {
     public void onClick(View v) {
       FragmentTransaction ft = getFragmentManager().beginTransaction();
-      
-      Fragment prev = getFragmentManager().findFragmentByTag(DatePickerDialogFragment.DATE_PICKER_ID);
+
+      Fragment prev = getFragmentManager().findFragmentByTag(
+          DatePickerDialogFragment.DATE_PICKER_ID);
       if (prev != null) {
         ft.remove(prev);
         firstActive = secondActive = false;
       }
       ft.addToBackStack(null);
-      
-      DialogFragment frag = new DatePickerDialogFragment(BetweenDatesFragment.this);
+
+      DialogFragment frag = new DatePickerDialogFragment(
+          BetweenDatesFragment.this);
       secondActive = true;
       frag.show(ft, DatePickerDialogFragment.DATE_PICKER_ID);
     }
   };
-  
+
+  /*
+   * Handles interaction with answer in spinner
+   */
+  private OnItemSelectedListener answerInSelectedListener = new OnItemSelectedListener() {
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+        long id) {
+      answerType = AnswerInType.values()[(int) id];
+      Log.v(TAG, "selected " + id + " type: " + answerType);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+      Log.v(TAG, "nothing selected");
+      answerType = AnswerInType.DAYS;
+    }
+  };
+
   /*
    * Handles click on the reset button
    */
-  private OnClickListener resetListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-    	firstDateInput.setText("");
-    	secondDateInput.setText("");
-    	answer.setText("");
+  private OnClickListener resetListener = new OnClickListener() {
+    public void onClick(View v) {
+      firstDateInput.setText("");
+      secondDateInput.setText("");
+      answer.setText("");
     }
   };
-  
+
   /*
    * Handles click on the submit button
    */
-  private OnClickListener submitListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
+  private OnClickListener submitListener = new OnClickListener() {
+    public void onClick(View v) {
       try {
         firstDate = firstDateInput.getText().toString();
         secondDate = secondDateInput.getText().toString();
-        
+
         Log.v(TAG, firstDate + " " + secondDate);
-                
-        int numDays = DateWrap.daysBetween(firstDate, secondDate);
-        if(numDays < 0) {
-          numDays = numDays * -1;
-        }
-        
-        Log.v(TAG, "numDays: " + numDays);
-        
-        boolean isWeeks;
-        if(daysWeeksSwitch.getClass().equals(ToggleButton.class)) {
-          isWeeks = ((ToggleButton)daysWeeksSwitch).isChecked();
-        } else {
-          isWeeks = ((Switch)daysWeeksSwitch).isChecked();
-        }
-        
-        if(isWeeks) {
-          Log.v(TAG, "weeks");
-          answer.setText(String.valueOf(numDays / 7));
-        } else {
-          Log.v(TAG, "days");
-          answer.setText(String.valueOf(numDays));
+
+        switch (answerType) {
+        case DAYS:
+          answer.setText(
+              String.valueOf(DateWrap.daysBetween(firstDate, secondDate)));
+          break;
+        case WEEKS:
+          answer.setText(
+              String.valueOf(DateWrap.weeksBetween(firstDate, secondDate)));
+          break;
+        case MONTHS:
+          answer.setText(
+              String.valueOf(DateWrap.monthsBetween(firstDate, secondDate)));
+          break;
+        case NATURAL:
+          answer.setText(DateWrap.naturalInterval(firstDate, secondDate));
+          break;
+//        case AGE:
+//          answer.setText(DateWrap.getAge(firstDate, secondDate));
+//          break;
         }
 
       } catch (ParseException e) {
         // XXX need to split this up, killing both is not nice
-        ((AndDayToDayActivity)getActivity()).showAlert(getString(R.string.date_error));
+        ((AndDayToDayActivity) getActivity())
+            .showAlert(getString(R.string.date_error));
         firstDateInput.setText("");
         secondDateInput.setText("");
       }
-      
+
     }
   };
-  
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
+
     firstActive = false;
     secondActive = false;
   }
@@ -172,28 +199,36 @@ public class BetweenDatesFragment extends SherlockFragment implements OnDateSetL
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View v = inflater.inflate(R.layout.between_dates, container, false);
-    
-    firstDateInput = (EditText)v.findViewById(R.id.first_date_input);
+
+    firstDateInput = (EditText) v.findViewById(R.id.first_date_input);
     firstDateInput.setFocusable(true);
     firstDateInput.requestFocus();
     firstDateInput.requestFocusFromTouch();
-    
-    secondDateInput = (EditText)v.findViewById(R.id.second_date_input);
-    daysWeeksSwitch = (Object)v.findViewById(R.id.days_weeks_switch);
-    answer = (TextView)v.findViewById(R.id.between_dates_answer);
-    
-    firstDateSelect = (ImageButton)v.findViewById(R.id.first_date_select);
+
+    secondDateInput = (EditText) v.findViewById(R.id.second_date_input);
+    answer = (TextView) v.findViewById(R.id.between_dates_answer);
+
+    answerInSpinner = (Spinner) v.findViewById(R.id.answer_in_spinner);
+    ArrayAdapter<CharSequence> answerInAdapter = ArrayAdapter
+        .createFromResource(getActivity(), R.array.answer_in_array,
+            android.R.layout.simple_spinner_item);
+    answerInAdapter
+        .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    answerInSpinner.setAdapter(answerInAdapter);
+    answerInSpinner.setOnItemSelectedListener(answerInSelectedListener);
+
+    firstDateSelect = (ImageButton) v.findViewById(R.id.first_date_select);
     firstDateSelect.setOnClickListener(firstDateListener);
-    
-    secondDateSelect = (ImageButton)v.findViewById(R.id.second_date_select);
+
+    secondDateSelect = (ImageButton) v.findViewById(R.id.second_date_select);
     secondDateSelect.setOnClickListener(secondDateListener);
-    
-    resetButton = (Button)v.findViewById(R.id.between_dates_reset_button);
+
+    resetButton = (Button) v.findViewById(R.id.between_dates_reset_button);
     resetButton.setOnClickListener(resetListener);
-    
-    submitButton = (Button)v.findViewById(R.id.between_dates_submit_button);
+
+    submitButton = (Button) v.findViewById(R.id.between_dates_submit_button);
     submitButton.setOnClickListener(submitListener);
-    
+
     return v;
   }
 
@@ -203,12 +238,12 @@ public class BetweenDatesFragment extends SherlockFragment implements OnDateSetL
   @Override
   public void onDateSet(DatePicker view, int year, int month, int day) {
     getFragmentManager().popBackStack();
-    if(firstActive) {
+    if (firstActive) {
       firstActive = false;
-      firstDateInput.setText(month+1 + "/" + day + "/" + year);
-    } else if(secondActive) {
+      firstDateInput.setText(month + 1 + "/" + day + "/" + year);
+    } else if (secondActive) {
       secondActive = false;
-      secondDateInput.setText(month+1 + "/" + day + "/" + year);
+      secondDateInput.setText(month + 1 + "/" + day + "/" + year);
     }
   }
 }
--- src/mobi/daytoday/DayToDay/DateWrap.java
+++ src/mobi/daytoday/DayToDay/DateWrap.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2012 Doyle Young
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2012 Doyle Young
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package mobi.daytoday.DayToDay;
 
 import java.text.DateFormat;
@@ -24,52 +24,231 @@ import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
+
+import android.util.Log;
+
 /**
  * Date math functions
+ * 
  * @author Doyle Young
- *
+ * 
  */
 public class DateWrap {
-  private DateWrap() {}
-  
+
+  private static final String TAG = "DateWrap";
+
   // constants for millisecond time calculation
   private static final int SECOND = 1000;
   private static final int MINUTE = 60 * SECOND;
   private static final int HOUR = 60 * MINUTE;
   private static final int DAY = 24 * HOUR;
-  
+
+  private static final String STR_DAY = "day";
+  private static final String STR_WEEK = "week";
+  private static final String STR_MONTH = "month";
+  private static final String STR_YEAR = "year";
+
   public static final String DATE_FORMAT = "MM/dd/yyyy";
-  
+  private static final DateFormat dateForm = new SimpleDateFormat(DATE_FORMAT,
+      Locale.getDefault());
+
+  private DateWrap() {
+  }
+
   /**
    * Add the number of days to the date and return the result
-   * @param theDate - date in DATE_FORMAT format
-   * @param numDays - number of days to add (may be negative for subtraction)
+   * 
+   * @param theDate
+   *          - date in DATE_FORMAT format
+   * @param numDays
+   *          - number of days to add (may be negative for subtraction)
    * @return result of the addition in DATE_FORMAT format
-   * @throws ParseException - if there is any error parsing the date
+   * @throws ParseException
+   *           - if there is any error parsing the date
    */
-  public static String addToDate(String theDate, int numDays) throws ParseException {
-    DateFormat dateForm = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+  public static String addToDate(String theDate, int numDays)
+      throws ParseException {
     Date date = dateForm.parse(theDate);
-    GregorianCalendar cal = new GregorianCalendar(TimeZone.getDefault(), Locale.getDefault());
+    GregorianCalendar cal = new GregorianCalendar(TimeZone.getDefault(),
+        Locale.getDefault());
     cal.setTime(date);
     cal.add(Calendar.DATE, numDays);
-    return (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.DATE) + "/" + cal.get(Calendar.YEAR);
+    return (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.DATE) + "/"
+        + cal.get(Calendar.YEAR);
   }
-  
+
   /**
    * Find the difference in days between the given dates and return the result
-   * @param dateOne - date in DATE_FORMAT format
-   * @param dateTwo - date in DATE_FORMAT format
+   * 
+   * @param dateOne
+   *          - date in DATE_FORMAT format
+   * @param dateTwo
+   *          - date in DATE_FORMAT format
    * @return number of days between the two given dates
-   * @throws ParseException - if there is any error parsing the date
+   * @throws ParseException
+   *           - if there is any error parsing a date
    */
-  public static int daysBetween(String dateOne, String dateTwo) throws ParseException {
-    DateFormat dateForm = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+  public static long daysBetween(String dateOne, String dateTwo)
+      throws ParseException {
     Date firstDate = dateForm.parse(dateOne);
     Date secondDate = dateForm.parse(dateTwo);
-    
+
     long difference = firstDate.getTime() - secondDate.getTime();
-    
-    return (int)(difference / DAY);
+
+    return (Math.abs(difference) / DAY);
   }
+
+  /**
+   * Find the difference in weeks between the given dates and return the result
+   * 
+   * @param dateOne
+   *          - date in DATE_FORMAT format
+   * @param dateTwo
+   *          - date in DATE_FORMAT format
+   * @return number of weeks between the two given dates
+   * @throws ParseException
+   *           - if there is any error parsing a date
+   */
+  public static long weeksBetween(String dateOne, String dateTwo)
+      throws ParseException {
+    return (daysBetween(dateOne, dateTwo) / 7);
+  }
+
+  /**
+   * Find the difference in months between the given dates and return the result
+   * 
+   * @param dateOne
+   *          - date in DATE_FORMAT format
+   * @param dateTwo
+   *          - date in DATE_FORMAT format
+   * @return number of months between the two given dates
+   * @throws ParseException
+   *           - if there is any error parsing a date
+   */
+  public static long monthsBetween(String dateOne, String dateTwo)
+      throws ParseException {
+    Calendar firstCal = Calendar.getInstance();
+    firstCal.setTime(dateForm.parse(dateOne));
+    Calendar secondCal = Calendar.getInstance();
+    secondCal.setTime(dateForm.parse(dateTwo));
+
+    // years to months
+    long months = Math.abs(firstCal.get(Calendar.YEAR)
+        - secondCal.get(Calendar.YEAR)) * 12;
+    // difference in months
+    months += Math.abs(firstCal.get(Calendar.MONTH)
+        - secondCal.get(Calendar.MONTH));
+
+    return months;
+  }
+
+  /**
+   * Find the difference between the two dates and express it in natural
+   * language
+   * 
+   * @param dateOne
+   *          - date in DATE_FORMAT format
+   * @param dateTwo
+   *          - date in DATE_FORMAT format
+   * @return time between the two given dates in natural terms
+   * @throws ParseException
+   *           - if there is any error parsing a date
+   */
+  public static String naturalInterval(String dateOne, String dateTwo)
+      throws ParseException {
+    DateTimeFormatter dtForm = DateTimeFormat.forPattern(DATE_FORMAT);   
+
+    DateTime firstDate = dtForm.parseDateTime(dateOne);
+    DateTime secondDate = dtForm.parseDateTime(dateTwo);
+
+    Period period = new Period(firstDate, secondDate);
+    
+    PeriodFormatter formatter = new PeriodFormatterBuilder()
+    .appendYears().appendSuffix(" year ", " years ")
+    .appendMonths().appendSuffix(" month ", " months ")
+    .appendWeeks().appendSuffix(" week ", " weeks ")
+    .appendDays().appendSuffix(" day", " days")
+    .printZeroNever()
+    .toFormatter();
+
+    return formatter.print(period);
+  }
+
+//  public static String getAge(String dateOne, String dateTwo)
+//      throws ParseException {
+//    StringBuilder age = new StringBuilder();
+//
+//    Calendar firstCal = Calendar.getInstance();
+//    firstCal.setTime(dateForm.parse(dateOne));
+//    Calendar secondCal = Calendar.getInstance();
+//    secondCal.setTime(dateForm.parse(dateTwo));
+//
+//    long monthsBetween = monthsBetween(dateOne, dateTwo);
+//    long years = monthsBetween / 12;
+//    long months = monthsBetween - (years * 12);
+//    Log.v(TAG, "years: " + years + " months: " + months);
+//
+//    if (years < 2) {
+//      if (months < 1) {
+//        long days = daysBetween(dateOne, dateTwo);
+//
+//        if (days < 14) {
+//          age.append(days);
+//          age.append(" ");
+//          age.append(STR_DAY);
+//
+//          if (days > 1) {
+//            age.append("s");
+//          }
+//        } else {
+//          long weeks = weeksBetween(dateOne, dateTwo);
+//          age.append(weeks);
+//          age.append(" ");
+//          age.append(STR_WEEK);
+//          age.append("s");
+//        }
+//      } else if (months < 2) {
+//        long weeks = weeksBetween(dateOne, dateTwo);
+//        age.append(weeks);
+//        age.append(" ");
+//        age.append(STR_WEEK);
+//        age.append("s");
+//      } else {
+//        long inMonths = monthsBetween(dateOne, dateTwo);
+//        age.append(inMonths);
+//        age.append(" ");
+//        age.append(STR_MONTH);
+//
+//        if (inMonths > 1) {
+//          age.append("s");
+//        }
+//      }
+//    } else if (years < 10) {
+//      if (months < 4) {
+//        age.append(years);
+//        age.append(" ");
+//        age.append(STR_YEAR);
+//        age.append("s");
+//      } else if (months < 10) {
+//        age.append(years);
+//        age.append(" and a half");
+//      } else {
+//        age.append("Almost ");
+//        age.append(years + 1);
+//      }
+//    } else {
+//      age.append(years);
+//      age.append(" ");
+//      age.append(STR_YEAR);
+//      age.append("s");
+//    }
+//
+//    return age.toString();
+//  }
 }
